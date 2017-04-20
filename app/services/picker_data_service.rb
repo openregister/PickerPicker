cae6ad32b3c487d9948016783d3fb82a789ec4c2
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2017-04-20 12:13:10 +0100

    Pipe user selections to script

--- app/services/picker_data_service.rb
+++ app/services/picker_data_service.rb
@@ -1,6 +1,27 @@
+require 'open3'
+
 class PickerDataService
   def generate(registerUri, fieldName)
-    path = Rails.root.join("app", "assets", "static", "picker-input.sample.json");
-    return File.read(path);
+    captured_stdout = ''
+    captured_stderr = ''
+
+    commands = "scripts/test.sh " + registerUri + " " + fieldName;
+    stdin, stdout, stderr, wait_thr = Open3.popen3(commands)
+
+    pid = wait_thr.pid  # pid of the started process.
+
+    stdin.close  # stdin, stdout and stderr should be closed explicitly in this form.
+
+    captured_stdout = stdout.read
+    captured_stderr = stderr.read
+
+    stdout.close
+    stderr.close
+    exit_status = wait_thr.value  # Process::Status object returned.
+
+    # path = Rails.root.join("app", "assets", "static", "picker-input.sample.json");
+    # return File.read(path);
+
+    return "stdout: " + captured_stdout + ", stderr: " + captured_stderr
   end
 end
---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Running script"
+
+filepath=$1
+field=$2
+
+echo "Path:" $filepath
+echo "Field:" $field
